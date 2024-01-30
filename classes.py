b4685e5d9c690671c1f93c8eeb075c94002b3fea
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-01-31 00:59:33 +0100

    Added game frame

--- classes.py
+++ classes.py
@@ -26,19 +26,30 @@ game_font = pygame.font.Font('fonts/MobileFont.ttf', 50)
 game_active = True
 
 
-def draw_dashed_line(start, end, height):
+def draw_dashed_line(start, end, v_distance):
     counter = 0
     while start + counter < end:
-        pygame.draw.line(background_surf, 'black', (start + counter, height), (start + counter + 3, height), 5)
+        pygame.draw.line(background_surf, 'black', (start + counter, v_distance), (start + counter + 3, v_distance), 5)
         counter += 5
 
+
+def draw_dashed_line_vertical(start, end, h_distance):
+    counter = 0
+    while h_distance + counter < end:
+        pygame.draw.line(background_surf, 'black', (start, h_distance + counter), (start+3, h_distance + counter), 5)
+        counter += 7
+# (40, 70) (41, 70)
+# (40, 71) (41, 71)
 
 background_surf = pygame.image.load('assets/background.png').convert()
 # game frame
 draw_dashed_line(40, 760, 70)
-draw_dashed_line(40, 760, 85)
+draw_dashed_line(40, 760, 87)
 draw_dashed_line(40, 760, 620)
-
+draw_dashed_line_vertical(40, 620, 94)
+draw_dashed_line_vertical(755, 620, 94)
+# pygame.draw.line(background_surf, 'black', (40, 94), (43, 94), 5)
+# pygame.draw.line(background_surf, 'black', (40, 101), (43, 101), 5)
 
 # score board
 score_sur = game_font.render('0036', False, 'black')
