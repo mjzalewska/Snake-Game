987fa5b550690606f321bc237581798b1a2445ad
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-02-01 23:00:44 +0100

    Refactored game setup code to OOP, filled basic config details to the Feed class

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 
 class Snake(pygame.sprite.Sprite):
@@ -6,6 +7,16 @@ class Snake(pygame.sprite.Sprite):
 
 
 class Feed(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/feed.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom=(100, 300))
+
+    def _set_location(self):
+        pass
+
+
+class IntroScreen:
     pass
 
 
@@ -13,56 +24,72 @@ class GameOverScreen:
     pass
 
 
-class Game:
-    pass
+class PixelFrame:
+    def __init__(self, surface):
+        self.surface = surface
+        self._draw_frame()
+
+    def _draw_horizontal_dashed_line(self, start: int, end: int, distance_from_top: int):
+        gap = 0
+        dash_len = 3
+        while start + gap < end:
+            pygame.draw.line(self.surface, 'black', (start + gap, distance_from_top),
+                             (start + gap + dash_len, distance_from_top), 5)
+            gap += 5
+
+    def _draw_vertical_dashed_line(self, start: int, end: int, distance_from_left: int):
+        gap = 0
+        dash_len = 3
+        while distance_from_left + gap < end:
+            pygame.draw.line(self.surface, 'black', (start, distance_from_left + gap),
+                             (start + dash_len, distance_from_left + gap), 5)
+            gap += 7
+
+    def _draw_frame(self):
+        self._draw_horizontal_dashed_line(40, 760, 70)
+        self._draw_horizontal_dashed_line(40, 760, 87)
+        self._draw_horizontal_dashed_line(40, 760, 620)
+        self._draw_vertical_dashed_line(40, 620, 94)
+        self._draw_vertical_dashed_line(755, 620, 94)
 
 
-# setup
-pygame.init()
-screen = pygame.display.set_mode((800, 672))
-pygame.display.set_caption("Retro Snake")
-clock = pygame.time.Clock()
-game_font = pygame.font.Font('fonts/MobileFont.ttf', 50)
-game_active = True
+class GameConfig:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 672))
+        self.background_surf = pygame.image.load('assets/background.png').convert()
+        self.game_frame = PixelFrame(self.background_surf)
+        self.game_font = pygame.font.Font('fonts/01Digit.ttf', 50)
+        self.score_board = None
+        self.score_rect = None
+        self._set_window_caption()
+        self.render_score_board('36') ## to do przemyślenia
+        self.clock = pygame.time.Clock()
+        self.game_active = True
+
+    @staticmethod
+    def _set_window_caption():
+        pygame.display.set_caption("Retro Snake")
+
+    def render_score_board(self, score):
+        self.score_board = self.game_font.render(score, False, 'black')
+        self.score_rect = self.score_board.get_rect(center=(95, 42))
 
 
-def draw_dashed_line(start, end, v_distance):
-    counter = 0
-    while start + counter < end:
-        pygame.draw.line(background_surf, 'black', (start + counter, v_distance), (start + counter + 3, v_distance), 5)
-        counter += 5
-
-
-def draw_dashed_line_vertical(start, end, h_distance):
-    counter = 0
-    while h_distance + counter < end:
-        pygame.draw.line(background_surf, 'black', (start, h_distance + counter), (start+3, h_distance + counter), 5)
-        counter += 7
-# (40, 70) (41, 70)
-# (40, 71) (41, 71)
-
-background_surf = pygame.image.load('assets/background.png').convert()
-# game frame
-draw_dashed_line(40, 760, 70)
-draw_dashed_line(40, 760, 87)
-draw_dashed_line(40, 760, 620)
-draw_dashed_line_vertical(40, 620, 94)
-draw_dashed_line_vertical(755, 620, 94)
-# pygame.draw.line(background_surf, 'black', (40, 94), (43, 94), 5)
-# pygame.draw.line(background_surf, 'black', (40, 101), (43, 101), 5)
-
-# score board
-score_sur = game_font.render('0036', False, 'black')
-score_rect = score_sur.get_rect(center=(110, 45))
+setup = GameConfig()
+feed = pygame.sprite.GroupSingle()
+feed.add(Feed())
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    if game_active:
-        screen.blit(background_surf, (0, 0))
-        screen.blit(score_sur, score_rect)
+    if setup.game_active:
+        setup.screen.blit(setup.background_surf, (0, 0))
+        setup.screen.blit(setup.score_board, setup.score_rect)
+        feed.draw(setup.screen)
 
     pygame.display.update()
-    clock.tick(60)
+    setup.clock.tick(60)
