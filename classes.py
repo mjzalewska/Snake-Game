fa32c2a89203c0ceecf27d6b7b27697bac5df5b9
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-04-01 23:37:34 +0200

    Refactored Snake class,update method

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 import pygame
 from random import randint
 
+GRIDSIZE = 21
+
 
 class IntroScreen:
     pass
@@ -12,39 +14,30 @@ class GameOverScreen:
 
 class Snake(pygame.sprite.Sprite):
     def __init__(self, group, position, length, parent=None):
-        super().__init__()
+        super().__init__(group)
         self.length = length
         self.parent = parent
         self.child = None
+        self.direction = 'UP'
         self.image = pygame.image.load('assets/element.png').convert_alpha()
         self.position = position
-        self.direction = (0, 0)
-        self.rect = self.image.get_rect(x=position[0], y=position[1])
+        self.rect = self.image.get_rect(x=self.position[0] * GRIDSIZE, y=self.position[1] * GRIDSIZE)
         if length > 1:
-            self.child = Snake(group, (position[0]+21, position[1]), self.length - 1, self)
-            group.add(self.child)
-
-    def grow(self, eat_feed: bool):
-        if eat_feed:
-            self.length += 1
-
-    def get_player_input(self):
-        if not self.parent:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP]:
-                self.direction = (0, -1)
-            elif keys[pygame.K_DOWN]:
-                self.direction = (0, 1)
-            elif keys[pygame.K_LEFT]:
-                self.direction = (-1, 0)
-            elif keys[pygame.K_RIGHT]:
-                self.direction = (1, 0)
+            self.child = Snake(group, (position[0], position[1] + 1), length - 1, self)
 
     def move(self):
         parent_direction = self.parent.direction if self.parent else None
-        new_position = self.position[0] + self.direction[0], self.position[1] + self.direction[1]
-        self.position = new_position
-        self.rect = self.image.get_rect(x=self.position[0], y=self.position[1])
+
+        if self.direction == 'UP':
+            self.position = self.position[0], self.position[1] - 1
+        elif self.direction == 'DOWN':
+            self.position = self.position[0], self.position[1] + 1
+        elif self.direction == 'LEFT':
+            self.position = self.position[0] - 1, self.position[1]
+        elif self.direction == 'RIGHT':
+            self.position = self.position[0] + 1, self.position[1]
+
+        self.rect = self.image.get_rect(x=self.position[0] * GRIDSIZE, y=self.position[1] * GRIDSIZE)
 
         if self.child:
             self.child.move()
@@ -52,16 +45,32 @@ class Snake(pygame.sprite.Sprite):
         if parent_direction:
             self.direction = parent_direction
 
-    def has_collision(self, other_object):
+    def grow(self, eat_feed: bool):
+        if eat_feed:
+            self.length += 1
+
+    def update(self):
+        if not self.parent:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                self.direction = 'UP'
+            elif keys[pygame.K_DOWN]:
+                self.direction = 'DOWN'
+            elif keys[pygame.K_LEFT]:
+                self.direction = 'LEFT'
+            elif keys[pygame.K_RIGHT]:
+                self.direction = 'RIGHT'
+
+    def is_collision(self, other_object):
         pass
         # self.rect.top > 87
         # self.rect.bottom < 620
         # self.rect.left > 44
         # self.rect.right < 758
 
-    def update(self):
-        self.get_player_input()
-        self.move()
+    # def update(self): # sprawdzić też to
+    #     self.get_direction()
+    # self.move()
 
 
 class Feed(pygame.sprite.Sprite):
@@ -96,11 +105,10 @@ class Frame:
             gap += 7
 
     def _draw_frame(self):
-        self._draw_horizontal_dashed_line(40, 760, 70)
-        self._draw_horizontal_dashed_line(40, 760, 87)
+        self._draw_horizontal_dashed_line(40, 760, 75)
         self._draw_horizontal_dashed_line(40, 760, 620)
-        self._draw_vertical_dashed_line(40, 620, 94)
-        self._draw_vertical_dashed_line(754, 620, 94)
+        self._draw_vertical_dashed_line(40, 620, 75)
+        self._draw_vertical_dashed_line(754, 620, 75)
 
 
 class GameConfig:
@@ -124,26 +132,29 @@ class GameConfig:
 
     def render_score_board(self, score):
         self.score_board = self.game_font.render(score, False, 'black')
-        self.score_rect = self.score_board.get_rect(center=(95, 42))
+        self.score_rect = self.score_board.get_rect(center=(70, 50))
 
 
 setup = GameConfig()
 
 snake_sprites = pygame.sprite.Group()
-snake = Snake(snake_sprites, (100, 400), 5)
+snake = Snake(snake_sprites, (10, 10), 4)
 snake_sprites.add(snake)
 
 feed_sprite = pygame.sprite.GroupSingle()
 feed = Feed()
 feed_sprite.add(feed)
 
-# key_press = 0
+MOVE = pygame.USEREVENT + 1
+pygame.time.set_timer(MOVE, 300)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == MOVE:
+            snake.move()
 
     if setup.game_active:
         setup.screen.blit(setup.background_surf, (0, 0))
@@ -152,5 +163,5 @@ while True:
         snake_sprites.update()
         snake_sprites.draw(setup.screen)
 
-    pygame.display.update()
     setup.clock.tick(60)
+    pygame.display.update()
